25c80ea57e99a2c8ab516b302ba77901eea8e7ee
julianjohannesen <github email address>
2018-12-04 15:43:02 -0500

    Got sample map loading

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+//import Marker from './components/marker.js'
+
+export default class Map extends Component {
+  render() {
+	return (
+	  <div>
+		Hello!
+	  </div>
+	)
+  }
+}
