78c86924a3ba1a310612b5ecf22dcbb241650bd1
Julian Johannesen <julian.johannesen@gmail.com>
2019-01-28 10:30:23 -0500

    Corrected footer to remove react-bulma-components

--- src/components/layout/Footerx.js
+++ src/components/layout/Footerx.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import Footer from 'react-bulma-components/lib/components/footer';
-import Container from 'react-bulma-components/lib/components/container';
-import Content from 'react-bulma-components/lib/components/content';
 
 export default function Footerx() {
 	return (
-		<Footer renderAs="footer">
-			<Container>
-				<Content style={{ textAlign: 'center' }}>
-					<p><strong>The Bars of Magoun Square</strong> by <a href="https://github.com/julianjohannesen">Julian Johannesen</a>.</p>
-				</Content>
-			</Container>
-		</Footer>
+		<footer className="footer">
+			<div className="content has-text-centered">
+				<p>
+					<strong>The Bars of Magoun Square</strong> by <a href="https://github.com/julianjohannesen">Julian Johannesen</a>.
+				</p>
+			</div>
+		</footer>
 	)
 }
