c10bebf6518f6c8541704307170e16b98d95375c
julianjohannesen <github email address>
2019-01-28 10:44:31 -0500

    Updates

---
+++ precache-manifest.1d0f23668cb4178036d38358b4958f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7283396794af44c99c6b6744d8c4cdc",
+    "url": "/bars-of-davis/static/media/oms-960x340.e7283396.jpg"
+  },
+  {
+    "revision": "76bc196b8d3f9096afca",
+    "url": "/bars-of-davis/static/js/runtime~main.76bc196b.js"
+  },
+  {
+    "revision": "4cdd9aa1c013f407cc95",
+    "url": "/bars-of-davis/static/js/main.4cdd9aa1.chunk.js"
+  },
+  {
+    "revision": "54c34e510d59d14a2193",
+    "url": "/bars-of-davis/static/js/1.54c34e51.chunk.js"
+  },
+  {
+    "revision": "4cdd9aa1c013f407cc95",
+    "url": "/bars-of-davis/static/css/main.eea34778.chunk.css"
+  },
+  {
+    "revision": "1de26972ee559f1d3c7cc396ee8e684b",
+    "url": "/bars-of-davis/index.html"
+  }
+];
