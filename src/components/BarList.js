9b97cc2c17ca6103dd3730d5a56a89d761d85991
Julian Johannesen <julian.johannesen@gmail.com>
2019-01-28 11:46:34 -0500

    listClick stash applied

--- src/components/BarList.js
+++ src/components/BarList.js
@@ -4,20 +4,30 @@ import uuid from 'uuid/v4';
 export default class BarList extends Component {
     render() {
 
+        const ulStyle = {
+            overflowY: "scroll",
+            height: "calc(100% - 3rem)",
+            width: "28vw",
+        }
+
         const itemStyle = {
             padding: "0.5em",
             border: "1px solid #242f3e",
             borderBottom: "none",
+            borderRight: "none",
         }
 
         return (
             <section id="barList" className="section column is-narrow" style={{ padding: "0", }}>
-                <ul className="menu-list">
-                    {this.props.barMarkers.map((marker)=> (
-                        <li className="menu-item" key={uuid()} style={itemStyle}>
-                            <h3>{marker.barData.venue.name}</h3>
-                        </li>
-                    ))}
+                <h2 className="heading is-size-4">Local Bars</h2>
+                <ul className="menu-list" id="barList" onClick={this.props.listClick} style={ulStyle}>
+                    {this.props.barMarkers.map((marker) => {
+                        return marker.visible ? (
+                            <li className="menu-item" key={uuid()} style={itemStyle}>
+                                <h3 id={marker.barData.venue.id}>{marker.barData.venue.name}</h3>
+                            </li>
+                        ) : null;
+                    })}
 
                 </ul>
             </section>
