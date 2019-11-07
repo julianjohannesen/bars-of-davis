4cc0498d9281fbf44d792a46af60f507229ff8a5
julianjohannesen <github email address>
2019-11-07 12:29:36 -0500

    updated packages, small fix to footer

--- src/components/layout/Footerx.js
+++ src/components/layout/Footerx.js
@@ -5,7 +5,7 @@ export default function Footerx() {
 		<footer className="footer">
 			<div className="content has-text-centered">
 				<p>
-					<strong>The Bars of Magoun Square</strong> by <a style={{textDecoration:"underline"}} href="https://github.com/julianjohannesen" title="My GitHub page">Julian Johannesen</a>.
+					<strong>The Bars of Davis Square</strong> by <a style={{textDecoration:"underline"}} href="https://github.com/julianjohannesen" title="My GitHub page">Julian Johannesen</a>.
 				</p>
 			</div>
 		</footer>
